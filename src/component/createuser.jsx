558e00c1b40de76b5a4db9a186872d5105d3135e
Sakshi Tripathi <sakshitripathi1411@gmail.com>
2021-01-17 04:08:20 +0530

    updated createuser file

--- src/component/createuser.jsx
+++ src/component/createuser.jsx
@@ -23,20 +23,25 @@ class CreateUser extends React.Component {
          
          if (this.state.fullname == '') {
              alert("full name cannot be empty")
+             return
          }
          if (isNaN(this.state.mobileno)||this.state.mobileno.length!=10) {
             alert("invalid mobile number")
+            return
         }
         if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(this.state.email)) {
             alert("invalid email id")
+            return
         }
 
         if (!/^[A-Za-z]\w{8,16}$/.test(this.state.password)) {
             alert("invalid password,try another")
+            return
         }
         if(this.state.confirmpassword!=this.state.password)
         {
             alert("passwords did not match")
+            return
         }
         
         this.callApi()
@@ -87,23 +92,26 @@ class CreateUser extends React.Component {
     render() {
       return (
         <div>
-      <TextField onChange={this.handleChange} id="fullname" label="fullname" variant="outlined" /><br></br>
-      <TextField onChange={this.handleChange} id="email" label="email" variant="outlined" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/><br></br>
-      
-      <input  onChange={this.handleChange} type="number" id="mobileno"  title="mobile number should be 10 digits" pattern="[1-9]{1}[0-9]{9}"/> <br></br>
+            <label for="fullname">Fullname:</label>
+      <input type="text" onChange={this.handleChange} id="fullname" label="fullname" variant="outlined" /><br></br><br></br>
+      <label for="email">Email:</label>
+      <input type="text" onChange={this.handleChange} id="email" label="email" variant="outlined" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/><br></br><br></br>
+      <label for="number">Mobile no.:</label>
+      <input  onChange={this.handleChange} type="number" id="mobileno"  title="mobile number should be 10 digits" pattern="[1-9]{1}[0-9]{9}"/> <br></br><br></br>
       <label for="pwd">Password:</label>
-      <input onChange={this.handleChange} type="password" id="password" /> <br></br>
+      <input onChange={this.handleChange} type="password" id="password" /> <br></br><br></br>
       <label for="confirmpass">Confirm Password:</label>
-      <input onChange={this.handleChange} type="password" id="confirmpassword" /> <br></br>
+      <input onChange={this.handleChange} type="password" id="confirmpassword" /> <br></br><br></br>
       <Button variant="outlined" onClick={this.onButtonClick}>Register</Button>
     <br/>
     <br/>
     <br/>
     <br/>
     <br/>
-    <TextField onChange={this.handleChange} id="loginemail" label="loginemail" variant="outlined" /><br></br>
+    <label for="email">Enter Email to login:</label>
+    <input type="text" onChange={this.handleChange} id="loginemail" label="loginemail" variant="outlined" /><br></br><br></br>
     <label for="pwd">Password:</label>
-      <input onChange={this.handleChange} type="password" id="loginpassword" /> 
+      <input onChange={this.handleChange} type="password" id="loginpassword" /> <br></br>
       <Button variant="outlined" onClick={this.onLoginButtonClick}>Login</Button>
     </div>
     
